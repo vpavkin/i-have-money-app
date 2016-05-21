8518d4bf9a3ecd1f30406fc07db7c748ccde80da
Vladimir Pavkin <vpavkin@gmail.com>
2016-05-21 22:56:16 +0300

    Setup read side tables for new projections

--- read-backend/src/main/resources/db/migrations/V1_0__ReadTables.sql
+++ read-backend/src/main/resources/db/migrations/V1_0__ReadTables.sql
@@ -1,6 +1,32 @@
 CREATE TABLE IF NOT EXISTS public.money (
   fortune_id VARCHAR(255) NOT NULL,
-  currency VARCHAR(255) NOT NULL,
-  amount NUMERIC NOT NULL,
-  PRIMARY KEY(fortune_id, currency)
+  currency   VARCHAR(255) NOT NULL,
+  amount     NUMERIC      NOT NULL,
+  PRIMARY KEY (fortune_id, currency)
+);
+
+CREATE TABLE IF NOT EXISTS public.stocks (
+  asset_id   VARCHAR(255) NOT NULL PRIMARY KEY,
+  fortune_id VARCHAR(255) NOT NULL,
+  name       VARCHAR(255) NOT NULL,
+  currency   VARCHAR(255) NOT NULL,
+  price      NUMERIC      NOT NULL,
+  count      NUMERIC      NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.realestate (
+  asset_id   VARCHAR(255) NOT NULL PRIMARY KEY,
+  fortune_id VARCHAR(255) NOT NULL,
+  name       VARCHAR(255) NOT NULL,
+  currency   VARCHAR(255) NOT NULL,
+  price      NUMERIC      NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.debts (
+  liability_id  VARCHAR(255) NOT NULL PRIMARY KEY,
+  fortune_id    VARCHAR(255) NOT NULL,
+  name          VARCHAR(255) NOT NULL,
+  currency      VARCHAR(255) NOT NULL,
+  amount        NUMERIC      NOT NULL,
+  interest_rate NUMERIC
 );
