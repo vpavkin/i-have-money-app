d47e1301a8395a2e6e9f2e28157b781612832562
Vladimir Pavkin <vpavkin@gmail.com>
2016-05-21 11:09:37 +0300

    Implement HTTP endpoints for new commands

--- frontend-protocol/shared/src/main/scala/ru/pavkin/ihavemoney/protocol/SharedProtocol.scala
+++ frontend-protocol/shared/src/main/scala/ru/pavkin/ihavemoney/protocol/SharedProtocol.scala
@@ -1,7 +1,8 @@
 package ru.pavkin.ihavemoney.protocol
 
 import cats.data.Xor
-import io.circe.{Decoder, Encoder, Json}
+import io.circe.{Decoder, DecodingFailure, Encoder, Json}
+import io.circe.syntax._
 import io.circe.generic.auto._
 import ru.pavkin.ihavemoney.domain.fortune.{Asset, Currency, Liability}
 
@@ -21,4 +22,19 @@ trait SharedProtocol {
 
   implicit val liabilityEncoder = Encoder[Liability]
   implicit val liabilityDecoder = Decoder[Liability]
+
+  implicit def currencyMapEncoder[T: Encoder]: Encoder[Map[Currency, T]] = Encoder.instance(_.map { case (k, v) ⇒ k.code -> v }.asJson)
+
+  implicit def currencyMapDecoder[T: Decoder]: Decoder[Map[Currency, T]] = Decoder.instance { c ⇒
+    Decoder.decodeMap[Map, T].apply(c)
+      .flatMap(_.foldLeft[Decoder.Result[Map[Currency, T]]](Xor.right(Map.empty[Currency, T])) {
+        case (m, (k, v)) ⇒ m.flatMap(mm ⇒
+          Currency.fromCode(k) match {
+            case Some(c) ⇒ Xor.right(mm + (c → v))
+            case None ⇒ Xor.left(DecodingFailure(s"$k is not a valid currency", Nil))
+          }
+        )
+      })
+  }
+
 }
