11b7f26e4ba0c23a3d91e0694ad363923f8bfd96
Vladimir Pavkin <vpavkin@gmail.com>
2016-05-22 18:11:48 +0300

    Fix findAll filter for inmemory repos

--- tests/src/main/scala/ru/pavkin/ihavemoney/domain/InMemoryAssetsViewRepository.scala
+++ tests/src/main/scala/ru/pavkin/ihavemoney/domain/InMemoryAssetsViewRepository.scala
@@ -8,7 +8,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class InMemoryAssetsViewRepository extends AssetsViewRepository with InMemoryRepository[(AssetId, FortuneId), Asset] {
 
   def findAll(id: FortuneId)(implicit ec: ExecutionContext): Future[Map[AssetId, Asset]] = Future.successful {
-    repo.filterKeys(_._1 == id)
+    repo.filterKeys(_._2 == id)
       .map { case ((a, f), r) ⇒ a → r }
   }
 }
--- tests/src/main/scala/ru/pavkin/ihavemoney/domain/InMemoryLiabilitiesViewRepository.scala
+++ tests/src/main/scala/ru/pavkin/ihavemoney/domain/InMemoryLiabilitiesViewRepository.scala
@@ -7,7 +7,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class InMemoryLiabilitiesViewRepository extends LiabilitiesViewRepository with InMemoryRepository[(LiabilityId, FortuneId), Liability] {
   def findAll(id: FortuneId)(implicit ec: ExecutionContext): Future[Map[LiabilityId, Liability]] = Future.successful {
-    repo.filterKeys(_._1 == id)
+    repo.filterKeys(_._2 == id)
       .map { case ((a, f), r) ⇒ a → r }
   }
 }
