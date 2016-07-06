462d6052cc003c457e720538335aa21e622ddce6
Vladimir Pavkin <vpavkin@gmail.com>
2016-07-06 17:13:38 +0300

    Add a bash script to push all images at once

---
+++ docker-push-all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker push vpavkin/ihavemoney-write-frontend
+docker push vpavkin/ihavemoney-write-backend
+docker push vpavkin/ihavemoney-read-frontend
+docker push vpavkin/ihavemoney-read-backend
