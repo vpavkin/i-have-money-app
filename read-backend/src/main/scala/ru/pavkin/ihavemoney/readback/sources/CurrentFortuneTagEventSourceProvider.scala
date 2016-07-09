16f5ed4367ad4bb896d5495234d32dce3553df5a
Vladimir Pavkin <vpavkin@gmail.com>
2016-07-09 16:27:29 +0300

    Eliminate JournalPuller as it's no longer needed with akka-persistence-jdbc 2.5.2. DRY Protobuf adapters

--- read-backend/src/main/scala/ru/pavkin/ihavemoney/readback/sources/CurrentFortuneTagEventSourceProvider.scala
+++ read-backend/src/main/scala/ru/pavkin/ihavemoney/readback/sources/CurrentFortuneTagEventSourceProvider.scala
@@ -2,20 +2,20 @@ package ru.pavkin.ihavemoney.readback.sources
 
 import akka.NotUsed
 import akka.actor.ActorContext
-import akka.persistence.jdbc.query.journal.scaladsl.JdbcReadJournal
+import akka.persistence.jdbc.query.scaladsl.JdbcReadJournal
 import akka.persistence.query.{EventEnvelope, PersistenceQuery}
 import akka.stream.scaladsl.Source
 import io.funcqrs.Tag
 import io.funcqrs.akka.EventsSourceProvider
+import ru.pavkin.ihavemoney.serialization.adapters.FortuneProtobufAdapter
 
 class CurrentFortuneTagEventSourceProvider(tag: Tag) extends EventsSourceProvider with FortuneProtobufAdapter {
 
   def source(offset: Long)(implicit context: ActorContext): Source[EventEnvelope, NotUsed] = {
 
     val journal = PersistenceQuery(context.system).readJournalFor[JdbcReadJournal](JdbcReadJournal.Identifier)
-
     journal.currentEventsByTag(tag.value, offset)
-      .map(e ⇒ e.copy(event = deserialize(e.event)))
+        .map(e ⇒ e.copy(event = deserialize(e.event)))
   }
 
 }
