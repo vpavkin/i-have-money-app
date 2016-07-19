2920ceaa81358b1f103f993ce1b6fb4b6d65ff12
Vladimir Pavkin <vpavkin@gmail.com>
2016-07-19 19:13:50 +0300

    Add yearly stats (for current year only for now)

--- js-app/src/main/scala/ru/pavkin/ihavemoney/frontend/components/StatsViewC.scala
+++ js-app/src/main/scala/ru/pavkin/ihavemoney/frontend/components/StatsViewC.scala
@@ -29,7 +29,8 @@ object StatsViewC {
       weekCurAgg: Currency,
       month: YearMonth,
       monthCurAgg: Currency,
-      year: Year)
+      year: Year,
+      yearCurAgg: Currency)
 
   class Backend($: BackendScope[Props, State]) {
 
@@ -52,6 +53,21 @@ object StatsViewC {
       pr.log().renderEmpty(PreloaderC()),
       pr.log().renderPending(_ => div(PreloaderC())),
       pr.log().renderReady { log ⇒
+
+        val yearExpensesByCategory = log
+            .filter(_.date.getYear == st.year.getValue)
+            .collect { case t: Expense => t }
+            .groupBy(_.category)
+
+        val yearExpensesAggregated =
+          yearExpensesByCategory
+              .mapValues(_.map {
+                case e if e.currency == st.yearCurAgg => -e.amount
+                case e if e.currency != st.yearCurAgg => AppCircuit.exchange(-e.amount, e.currency, st.yearCurAgg)
+              }.sum)
+
+        val yearWithTotal = yearExpensesAggregated.toList.sortBy(_._1) :+ (ExpenseCategory.total -> yearExpensesAggregated.values.sum)
+
         val monthlyExpensesByCategory = log
             .filter(e => e.date.getMonth == st.month.getMonth && e.date.getYear == st.month.getYear)
             .collect { case t: Expense => t }
@@ -85,34 +101,59 @@ object StatsViewC {
 
         div(common.container,
           div(grid.columnAll(GRID_SIZE / 2),
-            Panel(Some(div(
-              h3(common.panelTitle, "Weekly stats",
-                CurrencySelector(st.weekCurAgg, onChange = c => $.modState(_.copy(weekCurAgg = c)), addStyles = Seq(common.pullRight)),
-                span(" "),
-                WeekSelector(st.week, onChange = w => $.modState(_.copy(week = w)), addStyles = Seq(common.pullRight, rightMargin))
-              ))),
-              common.context.default,
-              table(className := "table table-striped table-hover table-condensed",
-                thead(tr(th("Category"), th("Expense"), th("Limit"), th("+/-"))),
-                tbody(
-                  weeklyWithTotal.map {
-                    case (cat, exp) ⇒
-                      val limitOpt = pr.fortune.weeklyLimits.get(ExpenseCategory(cat)).map(convertLimit(_, st.weekCurAgg))
-                      val overExp = limitOpt.map(_ - exp)
-                      tr(
-                        key := cat,
-                        td(
-                          if (overExp.exists(_ < 0)) span(className := "text-danger", Icon.exclamationTriangle, " ")
-                          else if (overExp.exists(_ < limitOpt.getOrElse(BigDecimal(0.0)) / 10))
-                            span(className := "text-warning", Icon.exclamationTriangle, " ")
-                          else EmptyTag,
-                          if (cat == ExpenseCategory.total) strong(cat) else cat
-                        ),
-                        td(logNegAmount, renderAmount(exp) + st.weekCurAgg.sign),
-                        td(limitOpt.map(l => renderAmount(l) + st.weekCurAgg.sign).getOrElse(" - "): String),
-                        td(overExp.map(amountStyle), overExp.map(o => renderAmount(o) + st.weekCurAgg.sign).getOrElse(" - "): String)
-                      )
-                  }
+            div(
+              Panel(Some(div(
+                h3(common.panelTitle, "Weekly stats",
+                  CurrencySelector(st.weekCurAgg, onChange = c => $.modState(_.copy(weekCurAgg = c)), addStyles = Seq(common.pullRight)),
+                  span(" "),
+                  WeekSelector(st.week, onChange = w => $.modState(_.copy(week = w)), addStyles = Seq(common.pullRight, rightMargin))
+                ))),
+                common.context.default,
+                table(className := "table table-striped table-hover table-condensed",
+                  thead(tr(th("Category"), th("Expense"), th("Limit"), th("+/-"))),
+                  tbody(
+                    weeklyWithTotal.map {
+                      case (cat, exp) ⇒
+                        val limitOpt = pr.fortune.weeklyLimits.get(ExpenseCategory(cat)).map(convertLimit(_, st.weekCurAgg))
+                        val overExp = limitOpt.map(_ - exp)
+                        tr(
+                          key := cat,
+                          td(
+                            if (overExp.exists(_ < 0)) span(className := "text-danger", Icon.exclamationTriangle, " ")
+                            else if (overExp.exists(_ < limitOpt.getOrElse(BigDecimal(0.0)) / 10))
+                              span(className := "text-warning", Icon.exclamationTriangle, " ")
+                            else EmptyTag,
+                            if (cat == ExpenseCategory.total) strong(cat) else cat
+                          ),
+                          td(logNegAmount, renderAmount(exp) + st.weekCurAgg.sign),
+                          td(limitOpt.map(l => renderAmount(l) + st.weekCurAgg.sign).getOrElse(" - "): String),
+                          td(overExp.map(amountStyle), overExp.map(o => renderAmount(o) + st.weekCurAgg.sign).getOrElse(" - "): String)
+                        )
+                    }
+                  )
+                )
+              )
+            ),
+            div(
+              Panel(Some(div(
+                h3(common.panelTitle, "Current year stats",
+                  CurrencySelector(st.monthCurAgg, onChange = c => $.modState(_.copy(monthCurAgg = c)), addStyles = Seq(common.pullRight))
+                ))),
+                common.context.default,
+                table(className := "table table-striped table-hover table-condensed",
+                  thead(tr(th("Category"), th("Expense"))),
+                  tbody(
+                    yearWithTotal.map {
+                      case (cat, exp) ⇒
+                        tr(
+                          key := cat,
+                          td(
+                            if (cat == ExpenseCategory.total) strong(cat) else cat
+                          ),
+                          td(logNegAmount, renderAmount(exp) + st.yearCurAgg.sign)
+                        )
+                    }
+                  )
                 )
               )
             )
@@ -156,7 +197,7 @@ object StatsViewC {
   }
 
   val component = ReactComponentB[Props]("StatsComponent")
-      .initialState(State(LocalDate.now().atStartOfWeek, Currency.EUR, YearMonth.now, Currency.EUR, Year.now))
+      .initialState(State(LocalDate.now().atStartOfWeek, Currency.EUR, YearMonth.now, Currency.EUR, Year.now, Currency.EUR))
       .renderBackend[Backend]
       .componentDidMount(s ⇒ s.backend.loadTransactionLog(s.props))
       .build
